0c5828fba59ad5b6dd48cf3e0c06b3361285bafb
Andrew Wong <andrew.wongsf@gmail.com>
2017-02-05 22:21:49 -0500

    added comments, removed csv file creation that was repetitive, and ran scraper.js again

--- scraper.js
+++ scraper.js
@@ -1,10 +1,18 @@
 /* CONTENT SCRAPER */
 
+// A web scraper that visits shirts4mike.com, follows links to all
+// t-shirts, and saves the product data in a CSV file.
+
 /* Node Modules */
 var fs = require("fs");
 var http = require("http");
 var cheerio = require("cheerio");
+// I chose to use cheerio to scrape content from the site because its selector implementation is 
+// identical to jQuery, which I am familiar with. Furthermore, it is a popular package with builds that 
+// are currently passing.  
 var json2csv = require("json2csv");
+// I chose to use json2csv to create a CSV file that stores the product data because it is also a popular
+// and active package with builds that are currently passing.
 
 /* Global Variables */
 var homePageURL = "http://www.shirts4mike.com/";
@@ -54,31 +62,37 @@ if (fs.existsSync("./data") === false) {
 
 }
 
-// 
+// Using HTTP module, make a GET request to website homepage
 http.get(homePageURL, (response) => {
 
+	// If the response is not successful, log an error message to the console.
 	if (response.statusCode !== 200) {
 
-		console.log("Error! The response status code is " + response.statusCode + ". Cannot access the home page!");	
+		console.log("Error! The response status code is " + response.statusCode + ". Cannot access the homepage!");	
 
+	// If the response is successful, pass the webpage's HTML to cheerio.
 	} else if (response.statusCode == 200) {
-				
+
 		response.on("data", (chunk) => {
 
     		var $ = cheerio.load(chunk);
 
+    		// Using cheerio, iterate over all links that contain '.php'
     		$("a[href*='.php']").each(function(){
 
     			var url = $(this).attr("href");
 
+    			// If the link contains '?id=' it is a product page, so make a GET request to the link and get product info
     			if (url.includes("?id=")) {
 
+    				// Add all product page links found on homepage to an array
     				productPageLinks.push(url);
 
     				http.get(homePageURL + url, (response) => {
 									
 						var requestURL = homePageURL + url;
 
+						// If the response is not successful, log an error message to the console.
 						if (response.statusCode !== 200) {
 
 							console.log("Error! The response status code is " + response.statusCode + ". The site you visited was http://www.shirts4mike.com/" + url);
@@ -87,8 +101,11 @@ http.get(homePageURL, (response) => {
 
 							response.on("data", (chunk) => {
 
+								// Pass product page's HTML to cheerio.
 								var $ = cheerio.load(chunk);
 
+								// Get the title, price, image URL, and current time
+								// Store them into object named shirtData
 								var Title = $("title").text();
 								var Price = $(".price").text();
 								var ImageURL = $(".shirt-picture > span > img").attr("src");
@@ -100,36 +117,37 @@ http.get(homePageURL, (response) => {
 								shirtData.ImageURL = homePageURL + ImageURL;
 								shirtData.URL = requestURL;
 								shirtData.Time = Time;
-								scrapeData.push(shirtData);
 
-								var csv = json2csv({ data: scrapeData, fields: columnHeaders });
-								 
-								fs.writeFile("./data/" + getDate() + ".csv", csv, function(error) {
-								  if (error) throw error;
-								  console.log("file saved");
-								});
+								// Add shirtData to globally accessible array
+								scrapeData.push(shirtData);
 							});
 						}
 					});
 
     			} else {
 
+    				// If the link doesn't contain '?id=', make a GET request to that link
     				http.get(homePageURL + url, (response) => {
 
+    					// If the response is not successful, log an error message to the console.
     					if (response.statusCode !== 200) {
 
     						console.log("Error! The response status code is " + response.statusCode + ".");
 
     					} else if (response.statusCode == 200) {
 
+    						// If the response is successful, pass the webpage's HTML to cheerio.
     						response.on("data", (chunk) => {
 
     							var $ = cheerio.load(chunk);
 
+    							// Using cheerio, iterate over all links that contain '.php'
     							$("a[href*='.php']").each(function(){
 
     								var url = $(this).attr("href");
 
+    								// If the link includes '?id=' and doesn't already exist in productPageLinks array,
+    								// make a GET request to that link and get product info
 					    			if (url.includes("?id=") && productPageLinks.indexOf(url) === -1) {
 					    				
 					    				productPageLinks.push(url);
@@ -138,6 +156,7 @@ http.get(homePageURL, (response) => {
 
 					    				http.get(requestURL, (response) => {
 
+					    					// If the response is not successful, log an error message to the console.
 											if (response.statusCode !== 200) {
 
 												console.log("Error! The response status code is " + response.statusCode + ". The site you visited was http://www.shirts4mike.com/" + url);
@@ -146,8 +165,11 @@ http.get(homePageURL, (response) => {
 
 												response.on("data", (chunk) => {
 
+													// Pass product page's HTML to cheerio.
 													var $ = cheerio.load(chunk);
 
+													// Get the title, price, image URL, and current time
+													// Store them into object named shirtData
 													var Title = $("title").text();
 													var Price = $(".price").text();
 													var ImageURL = $(".shirt-picture > span > img").attr("src");
@@ -159,10 +181,13 @@ http.get(homePageURL, (response) => {
 													shirtData.ImageURL = homePageURL + ImageURL;
 													shirtData.URL = requestURL;
 													shirtData.Time = Time;
+
+													// Add shirtData to globally accessible array
 													scrapeData.push(shirtData);
 
+													// Use json2csv to save scrapeData in a csv file
 													var csv = json2csv({ data: scrapeData, fields: columnHeaders });
-													 
+													
 													fs.writeFile("./data/" + getDate() + ".csv", csv, function(error) {
 													  if (error) throw error;
 													  console.log("file saved");
